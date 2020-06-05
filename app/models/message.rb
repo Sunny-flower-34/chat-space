f9a6a336658efc5bca3c0841bfc86da645061b22
Sunny-flower-34 <66055547+Sunny-flower-34@users.noreply.github.com>
2020-06-05 11:57:26 +0900

    Server-side implementation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,5 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
   validates :content, presence: true, unless: :image?
-          # contentカラムが空の場合は保存しない
   mount_uploader :image, ImageUploader
-
 end
