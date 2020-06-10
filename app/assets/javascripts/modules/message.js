c3251707de393cc2b424054fe914d11946a56358
Sunny-flower-34 <66055547+Sunny-flower-34@users.noreply.github.com>
2020-06-10 13:03:11 +0900

    add:automatic-update-function

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,9 +2,9 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="chat-room__message-list__message-index">
+        `<div class="chat-room__message-list__message-index" data-message-id=${message.id}>
           <div class="chat-room__message-list__message-index__set">
-            <div class="chat-room__message-list__message-index__set__name ">
+            <div class="chat-room__message-list__message-index__set__name">
               ${message.user_name}
             </div>
             <div class="chat-room__message-list__message-index__set__time">
@@ -21,47 +21,47 @@ $(function(){
       return html;
     } else {
       let html =
-        `<div class="chat-room__message-list__message-index">
-          <div class="chat-room__message-list__message-index__set">
-            <div class="chat-room__message-list__message-index__set__name ">
-              ${message.user_name}
-            </div>
-            <div class="chat-room__message-list__message-index__set__time">
-              ${message.created_at}
-            </div>
+      `<div class="chat-room__message-list__message-index" data-message-id=${message.id}>
+        <div class="chat-room__message-list__message-index__set">
+          <div class="chat-room__message-list__message-index__set__name">
+            ${message.user_name}
           </div>
-          <div class="chat-room__message-list__message-index__mess">
-            <p class="Message__content">
-              ${message.content}
-            </p>
+          <div class="chat-room__message-list__message-index__set__time">
+            ${message.created_at}
           </div>
-        </div>`
+        </div>
+        <div class="chat-room__message-list__message-index__mess">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
       return html;
     };
   }
-  $('.chat-room__message-form__contents').on('submit',function(e){
-    // console.log("Hello")
-    e.preventDefault()
+
+  $('.chat-room__message-form__contents').on('submit', function(e){
+    e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
     $.ajax({
-      url: url,  
-      type: 'POST',
-      data: formData,  
+      url: url,
+      type: "POST",
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data){
-      let html =  buildHTML(data);
+      let html = buildHTML(data);
       $('.chat-room__message-list').append(html);
       $('.chat-room__message-list').animate({ scrollTop: $('.chat-room__message-list')[0].scrollHeight});
       $('.chat-room__message-form__contents')[0].reset();
-      $('.form__submitbtn').prop('disabled', false);
+      $('.Form__submit').prop("disabled", false);
     })
     .fail(function() {
-      $('.form__submitbtn').prop('disabled', false);
       alert("メッセージ送信に失敗しました");
+      $('.Form__submit').prop("disabled", false);
     });
-  })
+  });
 });
